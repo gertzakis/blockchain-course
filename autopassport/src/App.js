1cfb1ca9f74228356fcaaa8f9d087d460498fab5
Gerasimos Tzakis <gertzakis@gmail.com>
2025-06-14 12:14:36 +0300

    add first draft of react app

---
+++ autopassport/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AutoPassport from './AutoPassport';
+
+function App() {
+  return (
+    <div className="App">
+      <AutoPassport/>
+    </div>
+  );
+}
+
+export default App;
